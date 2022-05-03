6028215276b1b51537d16b445c006f5e49e75e12
MieThorberg <cph-mt373@cphbusiness.dk>
2022-05-03 12:58:31 +0200

    added to endpoints

--- src/main/java/rest/GameResource.java
+++ src/main/java/rest/GameResource.java
@@ -1,6 +1,7 @@
 package rest;
 
 import com.google.gson.Gson;
+import dtos.CharacterDTO;
 import dtos.GameDTO;
 import dtos.UserDTO;
 import entities.Game;
@@ -8,10 +9,12 @@ import entities.Player;
 import entities.User;
 import facades.GameFacade;
 import utils.EMF_Creator;
+import utils.HttpUtils;
 
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
 @Path("game")
 public class GameResource {
@@ -45,6 +48,24 @@ public class GameResource {
         Game game = GameFacade.getGameFacade(EMF).getGameById(id);
         GameDTO gameDTO = new GameDTO(game);
         return GSON.toJson(gameDTO);
+    }
+
+    @GET
+    @Path("characters/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAll() throws IOException {
+        String data = HttpUtils.fetchData("https://miemt.me/werewolf_characters/api/characters/all");
+        CharacterDTO[] characterDTOS = GSON.fromJson(data, CharacterDTO[].class);
+        return GSON.toJson(characterDTOS);
+    }
+
+    @GET
+    @Path("characters/id/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getCharacterById(@PathParam("id") long id) throws IOException {
+        String data = HttpUtils.fetchData("https://miemt.me/werewolf_characters/api/characters/id/" + id);
+        CharacterDTO characterDTO = GSON.fromJson(data, CharacterDTO.class);
+        return GSON.toJson(characterDTO);
     }
 
 }
