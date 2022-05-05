ee6238cf14f45027cc747319d8c221c637e5d2c3
MieThorberg <cph-mt373@cphbusiness.dk>
2022-05-05 11:51:28 +0200

    refactor dto

--- src/main/java/dtos/CharacterDTO.java
+++ src/main/java/dtos/CharacterDTO.java
@@ -6,15 +6,20 @@ public class CharacterDTO {
     private String name;
     private String description;
     private String imageSource;
+    private int minPlayers;
+    private int max;
+    private String ability;
 
     public CharacterDTO() {
     }
 
-    public CharacterDTO(long id, String name, String description, String imageSource) {
-        this.id = id;
+    public CharacterDTO(String name, String description, String imageSource, int minPlayers, int max, String ability) {
         this.name = name;
         this.description = description;
         this.imageSource = imageSource;
+        this.minPlayers = minPlayers;
+        this.max = max;
+        this.ability = ability;
     }
 
     public long getId() {
@@ -47,5 +52,29 @@ public class CharacterDTO {
 
     public void setImageSource(String imageSource) {
         this.imageSource = imageSource;
+    }
+
+    public int getMinPlayers() {
+        return minPlayers;
+    }
+
+    public void setMinPlayers(int minPlayers) {
+        this.minPlayers = minPlayers;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public void setAbility(String ability) {
+        this.ability = ability;
     }
 }
