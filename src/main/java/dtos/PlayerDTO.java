c7a554da77ab7fed33d9257c006bfc2eeb1b3d1a
MieThorberg <cph-mt373@cphbusiness.dk>
2022-05-05 12:41:24 +0200

    added PlayerResource and PlayerFacade

--- src/main/java/dtos/PlayerDTO.java
+++ src/main/java/dtos/PlayerDTO.java
@@ -4,6 +4,9 @@ import entities.Game;
 import entities.Player;
 import entities.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerDTO {
 
     private String username;
@@ -23,5 +26,45 @@ public class PlayerDTO {
         this.characterId = player.getCharacterId();
         this.isAlive = player.getAlive();
         this.gameId = player.getGame().getId();
+    }
+
+    public static List<PlayerDTO> getPlayerDTOs(List<Player> players) {
+        List<PlayerDTO> playerDTOS = new ArrayList<>();
+        for (Player player : players) {
+            playerDTOS.add(new PlayerDTO(player));
+        }
+        return playerDTOS;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getCharacterId() {
+        return characterId;
+    }
+
+    public void setCharacterId(Long characterId) {
+        this.characterId = characterId;
+    }
+
+    public Boolean getAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(Boolean alive) {
+        isAlive = alive;
+    }
+
+    public Long getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(Long gameId) {
+        this.gameId = gameId;
     }
 }
