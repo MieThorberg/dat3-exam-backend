c9d80f2fb076e452016d69f466b8a81144e8a355
MieThorberg <cph-mt373@cphbusiness.dk>
2022-06-14 10:11:18 +0200

    added house entity + dto

---
+++ src/main/java/dtos/HouseDTO.java
@@ -0,0 +1,81 @@
+package dtos;
+
+import entities.House;
+import entities.Rental;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class HouseDTO {
+    private long id;
+    private String address;
+    private String city;
+    private int numberOfRooms;
+
+    public HouseDTO(House house) {
+        if(house.getId() != 0)
+            this.id = house.getId();
+        this.address = house.getAddress();
+        this.city = house.getAddress();
+        this.numberOfRooms = house.getNumberOfRooms();
+    }
+
+    public static Set<HouseDTO> getHouseDTOs(List<House> houses) {
+        Set<HouseDTO> houseDTOS = new HashSet<>();
+        houses.forEach(house -> houseDTOS.add(new HouseDTO(house)));
+        return houseDTOS;
+    }
+
+    public static Set<HouseDTO> getHouseDTOs(Set<House> houses) {
+        Set<HouseDTO> houseDTOS = new HashSet<>();
+        houses.forEach(house -> houseDTOS.add(new HouseDTO(house)));
+        return houseDTOS;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HouseDTO houseDTO = (HouseDTO) o;
+        return id == houseDTO.id && numberOfRooms == houseDTO.numberOfRooms && Objects.equals(address, houseDTO.address) && Objects.equals(city, houseDTO.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, address, city, numberOfRooms);
+    }
+}
---
+++ src/main/java/entities/House.java
@@ -0,0 +1,66 @@
+package entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "houses")
+@NamedQuery(name = "House.deleteAllRows", query = "DELETE from House")
+public class House implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "numberOfRooms")
+    private int numberOfRooms;
+
+    public House() {
+    }
+
+    public House(String address, String city, int numberOfRooms) {
+        this.address = address;
+        this.city = city;
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+}
